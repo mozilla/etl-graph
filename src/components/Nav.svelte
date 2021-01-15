1e7dc7c7acc75473f50d82232f7513e1512ba66d
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2021-01-14 18:51:17 -0800

    Move notes into the project itself

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,13 +1,18 @@
 <script>
     import { Nav, NavItem, NavLink } from "sveltestrap/src";
     export let segment;
-    let items = [{ href: "/", name: "home" }];
+    let items = [
+        { href: "/", name: "home" },
+        { href: "notes", name: "notes" },
+    ];
 </script>
 
 <Nav class="ml-auto" tabs>
     {#each items as item}
         <NavItem>
-            <NavLink active={items.name == segment} href={item.href}>
+            <NavLink
+                active={item.href == '/' ? !segment : item.name == segment}
+                href={item.href}>
                 {item.name}
             </NavLink>
         </NavItem>
---
+++ src/routes/notes/index.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Notes from "./Notes.md";
+</script>
+
+<Notes />
